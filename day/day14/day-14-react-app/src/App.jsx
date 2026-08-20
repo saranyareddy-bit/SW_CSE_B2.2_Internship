c33a2a5c967b7776f11de7dca5c10faed3d68f7d
saranyareddy-bit <sarareddy1310@gmail.com>
2026-08-21 01:34:38 +0530

    Event Example two

--- day/day14/day-14-react-app/src/App.jsx
+++ day/day14/day-14-react-app/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import ClickDemo from "./components/EventHandlerOne";
-
+import FormDemo from "./components/EventHandlerTwo";
 function App() {
   
 
   return (
     <>
       <ClickDemo />
+      <FormDemo />
         
     </>
   )
 }
 
-export default App
+export default App;
