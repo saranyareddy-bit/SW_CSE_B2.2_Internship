a581a0c57c3bcc96d073ebb8df69c5b4b0d0c5b9
saranyareddy-bit <sarareddy1310@gmail.com>
2026-08-20 18:49:23 +0530

    passing multiple ops

--- day/day13/day-13-react-app/src/App.jsx
+++ day/day13/day-13-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import { Welcome } from "./components/p1";
+import {ProductCard} from "./components/p2"
+import './App.css'
 function App() {
  
 
   return (
     <> 
     <Welcome name="Saranya"/>
+    <ProductCard
+    name="Samsung Galaxy M17 5G Mobile"
+    price={17999} 
+    image="https://m.media-amazon.com/images/I/714QJQZszoL._AC_UY327_FMwebp_QL65_.jpg" />
     </>
   )
 }
